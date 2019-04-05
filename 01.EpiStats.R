a9a1c978f4bf6bbbde5d0f9301dae2548032741b
smjenness <smjenness@gmail.com>
2019-04-05 16:48:08 -0400

    Update EpiStats through act rates

--- 01.EpiStats.R
+++ 01.EpiStats.R
@@ -9,7 +9,7 @@ rm(list = ls())
 library("tidyverse")
 
 ## Inputs ##
-city_name <- "San Francisco"
+city_name <- "Atlanta"
 
 ## Data ##
 library(ARTnetData)
@@ -33,27 +33,60 @@ l$diff.age <- abs(l$age - l$p_age)
 
 
 # Race
-race.cat2 <- ifelse(l$race.cat %in% c("white", "other"), 1, 0)
-p_race.cat2 <- ifelse(l$p_race.cat %in% c("white", "other"), 1, 0)
+table(l$race.cat, useNA = "always")
+table(l$p_race.cat, useNA = "always")
+table(l$race.cat, l$p_race.cat, useNA = "always")
+
+l$race.cat3 <- rep(NA, nrow(l))
+l$race.cat3[l$race.cat == "black"] <- 0
+l$race.cat3[l$race.cat == "hispanic"] <- 1
+l$race.cat3[l$race.cat %in% c("white", "other")] <- 2
+table(l$race.cat3, useNA = "always")
+
+table(l$p_race.cat, useNA = "always")
+l$p_race.cat3 <- rep(NA, nrow(l))
+l$p_race.cat3[l$p_race.cat == "black"] <- 0
+l$p_race.cat3[l$p_race.cat == "hispanic"] <- 1
+l$p_race.cat3[l$p_race.cat %in% c("white", "other")] <- 2
+table(l$p_race.cat3, useNA = "always")
+
+# redistribute NAs in proportion to non-missing partner races
+probs <- prop.table(table(l$race.cat3, l$p_race.cat3), 1)
+
+imp_black <- which(is.na(l$p_race.cat3) & l$race.cat3 == 0)
+l$p_race.cat3[imp_black] <- sample(0:2, length(imp_black), TRUE, probs[1, ])
+
+imp_hisp <- which(is.na(l$p_race.cat3) & l$race.cat3 == 1)
+l$p_race.cat3[imp_hisp] <- sample(0:2, length(imp_hisp), TRUE, probs[2, ])
+
+imp_white <- which(is.na(l$p_race.cat3) & l$race.cat3 == 2)
+l$p_race.cat3[imp_white] <- sample(0:2, length(imp_white), TRUE, probs[3, ])
+
+table(l$race.cat3, l$p_race.cat3, useNA = "always")
+
 l$race.combo <- rep(NA, nrow(l))
-l$race.combo[race.cat2 == 0 & p_race.cat2 == 0] <- 0
-l$race.combo[race.cat2 == 0 & p_race.cat2 == 1] <- 1
-l$race.combo[race.cat2 == 1 & p_race.cat2 == 0] <- 1
-l$race.combo[race.cat2 == 1 & p_race.cat2 == 1] <- 2
-table(race.cat2, p_race.cat2)
+l$race.combo[l$race.cat3 == 0 & l$p_race.cat3 == 0] <- 0
+l$race.combo[l$race.cat3 == 0 & l$p_race.cat3 %in% 1:2] <- 1
+l$race.combo[l$race.cat3 == 1 & l$p_race.cat3 %in% c(0, 2)] <- 2
+l$race.combo[l$race.cat3 == 1 & l$p_race.cat3 == 1] <- 3
+l$race.combo[l$race.cat3 == 2 & l$p_race.cat3 %in% 0:1] <- 4
+l$race.combo[l$race.cat3 == 2 & l$p_race.cat3 == 2] <- 5
+
 table(l$race.combo)
-l <- select(l, -c(race.cat, p_race.cat))
+l <- select(l, -c(race.cat3, p_race.cat3))
+
 
 # HIV
-table(l$hiv, l$p_hiv, useNA = "always")
+l$p_hiv2 <- ifelse(l$p_hiv == 1, 1, 0)
+table(l$hiv2, l$p_hiv, useNA = "always")
 
 hiv.combo <- rep(NA, nrow(l))
-hiv.combo[l$hiv == 0 & l$p_hiv == 0] <- 1
-hiv.combo[l$hiv == 1 & l$p_hiv == 1] <- 2
-hiv.combo[l$hiv == 1 & l$p_hiv == 0] <- 3
-hiv.combo[l$hiv == 0 & l$p_hiv == 1] <- 3
-hiv.combo[l$hiv == 0 & l$p_hiv == 2] <- 4
-hiv.combo[l$hiv == 1 & l$p_hiv == 2] <- 5
+hiv.combo[l$hiv2 == 0 & l$p_hiv == 0] <- 1
+hiv.combo[l$hiv2 == 1 & l$p_hiv == 1] <- 2
+hiv.combo[l$hiv2 == 1 & l$p_hiv == 0] <- 3
+hiv.combo[l$hiv2 == 0 & l$p_hiv == 1] <- 3
+hiv.combo[l$hiv2 == 0 & l$p_hiv == 2] <- 4
+hiv.combo[l$hiv2 == 1 & l$p_hiv == 2] <- 5
 table(hiv.combo, useNA = "always")
 
 l$hiv.combo <- hiv.combo
@@ -65,9 +98,9 @@ table(l$hiv.concord.pos)
 # PrEP
 names(d)
 table(d$PREP_REVISED, useNA = "always")
-table(d$PREP_CURRENT, useNA = "always")
-table(d$PREP_REVISED, d$PREP_CURRENT, useNA = "always")
-d$prep <- ifelse(d$PREP_CURRENT == 0 | is.na(d$PREP_CURRENT), 0, 1)
+table(d$artnetPREP_CURRENT, useNA = "always")
+table(d$PREP_REVISED, d$artnetPREP_CURRENT, useNA = "always")
+d$prep <- ifelse(d$artnetPREP_CURRENT == 0 | is.na(d$artnetPREP_CURRENT), 0, 1)
 table(d$prep, useNA = "always")
 
 dlim <- select(d, c(AMIS_ID, survey.year, prep))
@@ -81,17 +114,11 @@ d$cityYN <- ifelse(d$city2 == city_name, 1, 0)
 # Act Rates ---------------------------------------------------------------
 
 # acts/per week/per partnership for main and casual partnerships
-# fx of: partnership duration,
-#        age of each partner (take sum)
-#        race combo (in two-category set up, 3 values)
-#        HIV combo (same as race)
-#        partnership type
-#        geography main effect
 
 # Pull Data
 la <- select(l, ptype, duration, comb.age, city = cityYN,
              race.combo, RAI, IAI, hiv.concord.pos, prep,
-             acts = anal.acts.week, cp.acts) %>%
+             acts = anal.acts.week, anal.acts.week.cp) %>%
   filter(ptype %in% 1:2) %>%
   filter(RAI == 1 | IAI == 1)
 la <- select(la, -c(RAI, IAI))
@@ -107,7 +134,7 @@ summary(acts.mod)
 
 x <- expand.grid(duration = seq(0, 600, 50),
                  ptype = 1:2,
-                 race.combo = 0:2,
+                 race.combo = 0:5,
                  comb.age = seq(40, 120, 40),
                  hiv.concord.pos = 0:1,
                  city = 1)
