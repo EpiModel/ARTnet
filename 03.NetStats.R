e79464686bde0ddefe2fb47495e7ccc9d1af75b5
Samuel Jenness <smjenness@gmail.com>
2019-04-07 18:42:25 -0400

    Update 03 NetStats for 3-race setup

--- 03.NetStats.R
+++ 03.NetStats.R
@@ -1,22 +1,20 @@
 
 ##
-## Network stats calculator for ART-Net Data
-## v1: 2018-08
+## Network stats calculator for ARTnet Data
 ##
 
 ## Packages ##
-rm(list = ls())
 library("tidyverse")
-library("readxl")
-suppressPackageStartupMessages(library("EpiModelHIV"))
+library("EpiModelHIV")
+library("ARTnetData")
 
 
 ## Inputs ##
-city_name <- "San Francisco"
+city_name <- "Atlanta"
 coef_name <- paste0("city2", city_name)
 network_size <- 10000
 diss_nodematch <- TRUE
-edges_avg_nfrace <- TRUE
+edges_avg_nfrace <- FALSE
 
 
 ## Load Data ##
@@ -36,29 +34,34 @@ out$demog <- list()
 num <- network_size
 
 # Population size by race group
-# B = Black, Hispanic, Other
-# W = White, Asian
-rdist <- as.data.frame(read_excel("inst/RaceDistribution.xlsx"))
-prop.w <- rdist[which(rdist$City == city_name), 2]/100
-num.W <- out$demog$num.W <- round(num * prop.w)
-num.B <- out$demog$num.B <- num - num.W
+race.dist.3cat
+props <- race.dist.3cat[which(race.dist.3cat$City == city_name), -1]/100
 
-## Age-sex-specific mortality rates (B, W)
+num.B <- out$demog$num.B <- round(num * props$Black)
+num.H <- out$demog$num.H <- round(num * props$Hispanic)
+num.W <- out$demog$num.W <- num - num.B - num.H
+
+## Age-sex-specific mortality rates (B, H, W)
 #    in 5-year age decrments starting with age 15
+# TODO: update asmr.H for hispanics, currently just using asmr.B
 ages <- out$demog$ages <- 15:64
 asmr.B <- c(0.00078, 0.00148, 0.00157, 0.00168, 0.00198,
+            0.00254, 0.00376, 0.00628, 0.00999, 0.01533)
+asmr.H <- c(0.00078, 0.00148, 0.00157, 0.00168, 0.00198,
             0.00254, 0.00376, 0.00628, 0.00999, 0.01533)
 asmr.W <- c(0.00059, 0.00117, 0.00144, 0.00168, 0.00194,
             0.00249, 0.00367, 0.00593, 0.00881, 0.01255)
 
 # transformed to weekly rates
 trans.asmr.B <- 1 - (1 - asmr.B)^(1/52)
+trans.asmr.H <- 1 - (1 - asmr.H)^(1/52)
 trans.asmr.W <- 1 - (1 - asmr.W)^(1/52)
 
 # Null rate for 0-14, transformed rates, total rate for 65
 vec.asmr.B <- c(rep(0, 14), rep(trans.asmr.B, each = 5), 1)
+vec.asmr.H <- c(rep(0, 14), rep(trans.asmr.H, each = 5), 1)
 vec.asmr.W <- c(rep(0, 14), rep(trans.asmr.W, each = 5), 1)
-asmr <- data.frame(age = 1:65, vec.asmr.B, vec.asmr.W)
+asmr <- data.frame(age = 1:65, vec.asmr.B, vec.asmr.H, vec.asmr.W)
 
 out$demog$asmr <- asmr
 
@@ -78,7 +81,7 @@ attr_age.grp <- cut(attr_age, age.breaks, labels = FALSE)
 out$attr$age.grp <- attr_age.grp
 
 # race attribute
-attr_race <- apportion_lr(num, 0:1, c(num.B/num, num.W/num), shuffled = TRUE)
+attr_race <- apportion_lr(num, 0:2, c(num.B/num, num.H/num, num.W/num), shuffled = TRUE)
 out$attr$race <- attr_race
 
 # deg.casl attribute
@@ -90,7 +93,7 @@ attr_deg.main <- apportion_lr(num, 0:2, nstats$casl$deg.main.dist, shuffled = TR
 out$attr$deg.main <- attr_deg.main
 
 # deg tot 3 attribute
-attr_deg.tot <- apportion_lr(num, 0:3, nstats$inst$deg.tot.dist , shuffled = TRUE)
+attr_deg.tot <- apportion_lr(num, 0:3, nstats$inst$deg.tot.dist, shuffled = TRUE)
 out$attr$deg.tot <- attr_deg.tot
 
 # risk group
@@ -102,7 +105,7 @@ attr_role.class <- apportion_lr(num, 0:2, nstats$all$role.type, shuffled = TRUE)
 out$attr$role.class <- attr_role.class
 
 # diag status
-xs <- data.frame(age = attr_age, race2 = attr_race, cityYN = 1)
+xs <- data.frame(age = attr_age, race.cat3 = attr_race, cityYN = 1)
 preds <- predict(estats$hiv.mod, newdata = xs, type = "response")
 attr_diag.status <- rbinom(num, 1, preds)
 out$attr$diag.status <- attr_diag.status
@@ -119,30 +122,34 @@ if (edges_avg_nfrace == FALSE) {
   out$main$edges <- sum(unname(table(out$attr$race)) * nstats$main$nf.race)/2
 }
 
-# 1B: nodematch("age.grp")
-out$main$nodematch_age.grp <- out$main$edges *
-  (unname(table(out$attr$age.grp))/num) * nstats$main$nm.age.grp
+# 1B: nodefactor("age.grp")
+nodefactor_age.grp <- table(out$attr$age.grp) * nstats$main$nf.age.grp
+out$main$nodefactor_age.grp <- unname(nodefactor_age.grp)
 
-# 1C: nodefactor("age.grp")
-out$main$nodefactor_age.grp <- unname(table(out$attr$age.grp)) * nstats$main$nf.age.grp
+# 1C: nodematch("age.grp")
+nodematch_age.grp <- nodefactor_age.grp/2 * nstats$main$nm.age.grp
+out$main$nodematch_age.grp <- unname(nodematch_age.grp)
 
-# 1D: nodematch("race")
-out$main$nodematch_race <- out$main$edges * nstats$main$nm.race
+# 1D: nodefactor("race")
+nodefactor_race <- table(out$attr$race) * nstats$main$nf.race
+out$main$nodefactor_race <- unname(nodefactor_race)
 
-# 1E: nodefactor("race")
-out$main$nodefactor_race <- unname(table(out$attr$race)) * nstats$main$nf.race
+# 1E: nodematch("race")
+nodematch_race <- nodefactor_race/2 * nstats$main$nm.race
+out$main$nodematch_race <- unname(nodematch_race)
 
 # 1F: nodefactor("deg.casl")
-out$main$nodefactor_deg.casl <- num * nstats$main$deg.casl.dist * nstats$main$md.main.casl
+out$main$nodefactor_deg.casl <- num * nstats$main$deg.casl.dist * nstats$main$nf.deg.casl
 
 # 1G: concurrent
 out$main$concurrent <- num * nstats$main$concurrent
 
 # 1H: nodefactor("diag.status")
-out$main$nodefactor_diag.status <- unname(table(out$attr$diag.status)) * nstats$main$nf.diag.status
+nodefactor_diag.status <- table(out$attr$diag.status) * nstats$main$nf.diag.status
+out$main$nodefactor_diag.status <- unname(nodefactor_diag.status)
 
 # Dissolution
-exp.mort <- (mean(trans.asmr.B) + mean(trans.asmr.W)) / 2
+exp.mort <- (mean(trans.asmr.B) + mean(trans.asmr.H) + mean(trans.asmr.W)) / 3
 if (diss_nodematch == FALSE) {
   out$main$diss <- dissolution_coefs(dissolution = ~offset(edges),
                                      duration = nstats$main$durs.main.homog$mean.dur.adj,
@@ -152,7 +159,6 @@ if (diss_nodematch == FALSE) {
                                      duration = nstats$main$durs.main.byage$mean.dur.adj,
                                      d.rate = exp.mort)
 }
-
 
 
 # 2. Casual Partnership Stats ------------------------------------------------
@@ -166,30 +172,33 @@ if (edges_avg_nfrace == FALSE) {
   out$casl$edges <- sum(unname(table(out$attr$race)) * nstats$casl$nf.race)/2
 }
 
-# 2B: nodematch("age.grp")
-out$casl$nodematch_age.grp <- out$casl$edges *
-  (unname(table(out$attr$age.grp))/num) * nstats$casl$nm.age.grp
+# 2B: nodefactor("age.grp")
+nodefactor_age.grp <- table(out$attr$age.grp) * nstats$casl$nf.age.grp
+out$casl$nodefactor_age.grp <- unname(nodefactor_age.grp)
 
-# 2C: nodefactor("age.grp")
-out$casl$nodefactor_age.grp <- unname(table(out$attr$age.grp)) * nstats$casl$nf.age.grp
+# 2C: nodematch("age.grp")
+nodematch_age.grp <- nodefactor_age.grp/2 * nstats$casl$nm.age.grp
+out$casl$nodematch_age.grp <- unname(nodematch_age.grp)
 
-# 2D: nodematch("race")
-out$casl$nodematch_race <- out$casl$edges * nstats$casl$nm.race
+# 2D: nodefactor("race")
+nodefactor_race <- table(out$attr$race) * nstats$casl$nf.race
+out$casl$nodefactor_race <- unname(nodefactor_race)
 
-# 2E: nodefactor("race")
-out$casl$nodefactor_race <- unname(table(out$attr$race)) * nstats$casl$nf.race
+# 2E: nodematch("race")
+nodematch_race <- nodefactor_race/2 * nstats$casl$nm.race
+out$casl$nodematch_race <- unname(nodematch_race)
 
 # 2F: nodefactor("deg.main")
-out$casl$nodefactor_deg.main <- num * nstats$casl$deg.main.dist * nstats$casl$md.casl.main
+out$casl$nodefactor_deg.main <- num * nstats$casl$deg.main.dist * nstats$casl$nf.deg.main
 
 # 2G: concurrent
 out$casl$concurrent <- num * nstats$casl$concurrent
 
 # 2H: nodefactor("diag.status")
-out$casl$nodefactor_diag.status <- unname(table(out$attr$diag.status)) * nstats$casl$nf.diag.status
+nodefactor_diag.status <- table(out$attr$diag.status) * nstats$casl$nf.diag.status
+out$casl$nodefactor_diag.status <- unname(nodefactor_diag.status) 
 
 # Dissolution
-exp.mort <- (mean(trans.asmr.B) + mean(trans.asmr.W)) / 2
 if (diss_nodematch == FALSE) {
   out$casl$diss <- dissolution_coefs(dissolution = ~offset(edges),
                                      duration = nstats$casl$durs.casl.homog$mean.dur.adj,
@@ -212,29 +221,33 @@ if (edges_avg_nfrace == FALSE) {
   out$inst$edges <- sum(unname(table(out$attr$race)) * nstats$inst$nf.race)/2
 }
 
-# 3B: nodematch("age.grp", diff = FALSE)
-out$inst$nodematch_age.grp <- sum(out$inst$edges *
-                                    (unname(table(out$attr$age.grp))/num) * nstats$inst$nm.age.grp)
+# 3B: nodefactor("age.grp")
+nodefactor_age.grp <- table(out$attr$age.grp) * nstats$inst$nf.age.grp
+out$inst$nodefactor_age.grp <- unname(nodefactor_age.grp)
 
-# 3C: nodefactor("age.grp")
-out$inst$nodefactor_age.grp <- unname(table(out$attr$age.grp)) * nstats$inst$nf.age.grp
+# 3C: nodematch("age.grp")
+nodematch_age.grp <- nodefactor_age.grp/2 * nstats$inst$nm.age.grp
+out$inst$nodematch_age.grp <- unname(nodematch_age.grp)
 
-# 3D: nodematch("race", diff = FALSE)
-out$inst$nodematch_race <- out$inst$edges * nstats$inst$nm.race
+# 3D: nodefactor("race")
+nodefactor_race <- table(out$attr$race) * nstats$inst$nf.race
+out$inst$nodefactor_race <- unname(nodefactor_race)
 
-# 3E: nodefactor("race")
-out$inst$nodefactor_race <- unname(table(out$attr$race)) * nstats$inst$nf.race
+# 3E: nodematch("race")
+nodematch_race <- nodefactor_race/2 * nstats$inst$nm.race
+out$inst$nodematch_race <- unname(nodematch_race)
 
 # 3F: nodefactor("risk.grp")
+nodefactor_risk.grp <- table(out$attr$risk.grp) * nstats$inst$nf.risk.grp
+out$inst$nodefactor_risk.grp <- unname(nodefactor_risk.grp)
 
-out$inst$nodefactor_risk.grp <- unname(table(out$attr$risk.grp)) * nstats$inst$nf.risk.grp
-
-# 3G: nodefactor("deg.tot(3+)")
-
-out$inst$nodefactor_deg.tot <- unname(table(out$attr$deg.tot)) * nstats$inst$nf.deg.tot
+# 3G: nodefactor("deg.tot")
+nodefactor_deg.tot <- table(out$attr$deg.tot) * nstats$inst$nf.deg.tot
+out$inst$nodefactor_deg.tot <- unname(nodefactor_deg.tot) * nstats$inst$nf.deg.tot
 
 # 3H: nodefactor("diag.status")
-out$inst$nodefactor_diag.status <- unname(table(out$attr$diag.status)) * nstats$inst$nf.diag.status
+nodefactor_diag.status <- table(out$attr$diag.status) * nstats$inst$nf.diag.status
+out$inst$nodefactor_diag.status <- unname(nodefactor_diag.status)
 
 
 # Save Out File -----------------------------------------------------------
