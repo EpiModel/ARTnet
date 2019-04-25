23a1af24f02dbf9b9584b1b2a06092d0f60d92bd
smjenness <smjenness@gmail.com>
2019-04-25 13:14:56 -0400

    Use new levels parameterization for nodefactor terms

--- 05.Diagnostics.R
+++ 05.Diagnostics.R
@@ -27,17 +27,17 @@ fit_main <- est[[1]]
 
 model_main_dx <- ~edges +
   nodematch("age.grp", diff = TRUE) +
-  nodefactor("age.grp", base = 0) +
+  nodefactor("age.grp", levels = 1:5) +
   nodematch("race", diff = TRUE) +
-  nodefactor("race", base = 0) +
-  nodefactor("deg.casl", base = 0) +
+  nodefactor("race", levels = 1:3) +
+  nodefactor("deg.casl", levels = 0:3) +
   degrange(from = 3) +
   concurrent +
-  nodefactor("diag.status", base = 0) +
-  nodematch("role.class", diff = TRUE, keep = 1:2) +
+  nodefactor("diag.status", levels = 0:1) +
+  nodematch("role.class", diff = TRUE, levels = 1:3) +
   degree(0:3)
 dx_main <- netdx(fit_main, nsims = 10, ncores = 6, nsteps = 500,
-            nwstats.formula = model_main_dx,
+            nwstats.formula = model_main_dx, skip.dissolution = TRUE,
             set.control.ergm = control.simulate.ergm(MCMC.burnin = 1e5))
 print(dx_main)
 plot(dx_main)
@@ -51,17 +51,17 @@ fit_casl <- est[[2]]
 
 model_casl_dx <- ~edges +
   nodematch("age.grp", diff = TRUE) +
-  nodefactor("age.grp", base = 0) +
+  nodefactor("age.grp", levels = 1:5) +
   nodematch("race", diff = TRUE) +
-  nodefactor("race", base = 0) +
-  nodefactor("deg.main", base = 0) +
+  nodefactor("race", levels = 1:3) +
+  nodefactor("deg.main", levels = 0:2) +
   degrange(from = 4) +
   concurrent +
-  nodefactor("diag.status", base = 0) +
-  nodematch("role.class", diff = TRUE, keep = 1:2) +
+  nodefactor("diag.status", levels = 0:1) +
+  nodematch("role.class", diff = TRUE, levels = 1:3) +
   degree(0:4)
 dx_casl <- netdx(fit_casl, nsims = 10, ncores = 6, nsteps = 500,
-            nwstats.formula = model_casl_dx,
+            nwstats.formula = model_casl_dx, skip.dissolution = TRUE,
             set.control.ergm = control.simulate.ergm(MCMC.burnin = 1e5))
 print(dx_casl)
 plot(dx_casl)
@@ -75,20 +75,20 @@ fit_inst <- est[[3]]
 
 model_inst_dx <- ~edges +
   nodematch("age.grp", diff = FALSE) +
-  nodefactor("age.grp", base = 0) +
+  nodefactor("age.grp", levels = 1:5) +
   nodematch("race", diff = TRUE) +
-  nodefactor("race", base = 0) +
-  nodefactor("risk.grp", base = 0) +
-  nodefactor("deg.tot", base = 0) +
-  nodefactor("diag.status", base = 0) +
-  nodematch("role.class", diff = TRUE, keep = 1:2) +
+  nodefactor("race", levels = 1:3) +
+  nodefactor("risk.grp", levels = 1:5) +
+  nodefactor("deg.tot", levels = 0:5) +
+  nodefactor("diag.status", levels = 0:1) +
+  nodematch("role.class", diff = TRUE, levels = 1:3) +
   degree(0:4)
-
 dx_inst <- netdx(fit_inst, nsims = 10000, dynamic = FALSE,
             nwstats.formula = model_inst_dx,
             set.control.ergm = control.simulate.ergm(MCMC.burnin = 1e5))
 
 print(dx_inst)
-plot(dx_inst, sim.lines = FALSE)
+
+plot(dx_inst, sim.lines = TRUE, sim.lwd = 0.05)
 
 nstats$inst
