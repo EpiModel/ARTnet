592abbb67bf0929f8e8d25299f91ad63d79bf514
smjenness <smjenness@gmail.com>
2019-05-23 11:57:23 -0400

    Start package infrastructure

---
+++ R/ARTnet-package.R
@@ -0,0 +1,20 @@
+
+#' Epidemic and Network Model Parameterization with the ARTnet Dataset
+#'
+#' \tabular{ll}{
+#'    Package: \tab ARTnet\cr
+#'    Type: \tab Package\cr
+#'    Version: \tab 1.0.0\cr
+#'    Date: \tab 2019-05-23\cr
+#'    License: \tab GPL-3\cr
+#'    LazyLoad: \tab yes\cr
+#' }
+#'
+#' @name ARTnet-package
+#' @aliases ARTnet
+#' @import ARTnetData EpiModel tidyverse
+#'
+#' @docType package
+#' @keywords package
+#'
+NULL
