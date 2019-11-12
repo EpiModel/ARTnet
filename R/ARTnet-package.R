571a872d1fd099b4a0d80f22bf70bb1f784edf62
smjenness <smjenness@gmail.com>
2019-11-12 12:00:51 -0500

    Bump version/date

--- R/ARTnet-package.R
+++ R/ARTnet-package.R
@@ -4,8 +4,8 @@
 #' \tabular{ll}{
 #'    Package: \tab ARTnet\cr
 #'    Type: \tab Package\cr
-#'    Version: \tab 1.0.0\cr
-#'    Date: \tab 2019-05-23\cr
+#'    Version: \tab 2.0.0\cr
+#'    Date: \tab 2019-11-12\cr
 #'    License: \tab GPL-3\cr
 #'    LazyLoad: \tab yes\cr
 #' }
@@ -22,7 +22,6 @@
 #' @details
 #' The ARTnet package provides a suite of functions for the parameterization
 #' of epidemic and network models using data from the ARTnet cross-sectional survery of men
-#' who have sex with men (MSM) in the United States. Three functions make up this workflow
-#'  and are described below:
+#' who have sex with men (MSM) in the United States.
 #'
 NULL
