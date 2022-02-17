61c5ede2535ca4f7f40b8148851ddfe6cca2f74d
Samuel Jenness <smjenness@gmail.com>
2022-02-17 13:17:52 -0500

    Bump version/date: v2.5.6

--- R/ARTnet-package.R
+++ R/ARTnet-package.R
@@ -4,8 +4,8 @@
 #' \tabular{ll}{
 #'    Package: \tab ARTnet\cr
 #'    Type: \tab Package\cr
-#'    Version: \tab 2.5.5\cr
-#'    Date: \tab 2022-02-16\cr
+#'    Version: \tab 2.5.6\cr
+#'    Date: \tab 2022-02-17\cr
 #'    License: \tab GPL-3\cr
 #'    LazyLoad: \tab yes\cr
 #' }
