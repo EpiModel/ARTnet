fc18065ff75597140392c9da87afad1a9eba9e34
Samuel Jenness <samuel.m.jenness@emory.edu>
2022-11-11 11:57:54 -0500

    Bump version/date: v2.7.0

--- R/ARTnet-package.R
+++ R/ARTnet-package.R
@@ -5,7 +5,7 @@
 #'    Package: \tab ARTnet\cr
 #'    Type: \tab Package\cr
 #'    Version: \tab 2.7.0\cr
-#'    Date: \tab 2022-11-01\cr
+#'    Date: \tab 2022-11-11\cr
 #'    License: \tab GPL-3\cr
 #'    LazyLoad: \tab yes\cr
 #' }
@@ -20,8 +20,8 @@
 #' @keywords package
 #'
 #' @details
-#' The ARTnet package provides a suite of functions for the parameterization
-#' of epidemic and network models using data from the ARTnet cross-sectional
-#' survey of men who have sex with men (MSM) in the United States.
+#' The ARTnet package provides a suite of functions for the parameterization of epidemic and network
+#' models using data from the ARTnet cross-sectional survey of men who have sex with men (MSM) in
+#' the United States.
 #'
 NULL
